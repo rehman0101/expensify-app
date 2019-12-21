f9fa8b9ff9a12ecfdd6200a6ea40f3f496f411e0
rehman <umt12003065317@gmail.com>
2019-12-21 11:21:23 +0500

    removed unused import

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routes/AppRouter'
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 import { Provider } from 'react-redux';
 import 'normalize.css/normalize.css';
 import 'react-dates/lib/css/_datepicker.css';
